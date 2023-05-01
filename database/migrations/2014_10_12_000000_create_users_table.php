05ff6f18b0b52cc19a23333548f42acfe1395862
Alija Ghimire <alijaghimire27@gmail.com>
2023-05-01 13:50:04 +0545

    added fields in user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,9 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phonenumber');
+            $table->string('address');
+            $table->date('date_of_birth');
             $table->rememberToken();
             $table->timestamps();
         });
