277478ffcc7f36a4ba0a61e6649fa81c944dd66c
Alija Ghimire <alijaghimire27@gmail.com>
2023-05-01 23:00:51 +0545

    login function created in auth

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers\API\Auth;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
 
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    
+    public function login(Request $request){
+        $user=User::where('email',$request->email)->first();
+        if(!$user || !Hash::check($request->password,$user->password)){
+            throw ValidationException::withMessages([
+                "email"=>"The provided email is not correct.",
+                "password"=>"The provided password is not correct",
+            ]);
+        }
+        return response()->json([
+            "token"=>$user->createToken(Str::random(10))->plainTextToken,
+            "user"=>$user,
+        ]);
+        
+    }
 }
