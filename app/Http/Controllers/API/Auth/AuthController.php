b4f67f1015c981bace0b61c9d387aee47c489b41
Alija Ghimire <alijaghimire27@gmail.com>
2023-05-03 13:53:58 +0545

    validate request email and password in login

--- app/Http/Controllers/API/Auth/AuthController.php
+++ app/Http/Controllers/API/Auth/AuthController.php
@@ -13,6 +13,12 @@ use Illuminate\Validation\ValidationException;
 class AuthController extends Controller
 {
     public function login(Request $request){
+
+        $request->validate([
+            "email"=>"required|email",
+            "password"=>"required"
+        ]);
+        
         $user=User::where('email',$request->email)->first();
         if(!$user || !Hash::check($request->password,$user->password)){
             throw ValidationException::withMessages([
