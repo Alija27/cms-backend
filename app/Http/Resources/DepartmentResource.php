95b9eb4f81a685a415cc7652333ae0471b1bc520
Alija Ghimire <alijaghimire27@gmail.com>
2023-05-15 15:46:13 +0545

    removed field from resource

--- app/Http/Resources/DepartmentResource.php
+++ app/Http/Resources/DepartmentResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -17,7 +18,6 @@ class DepartmentResource extends JsonResource
         return [
             "id" => $this->id,
             "name" => ucwords($this->name),
-            "created_date" => $this->created_at
         ];
     }
 }
