4a0dd1898fe4515cfa16565b55b531606414a601
Alija Ghimire <alijaghimire27@gmail.com>
2023-05-24 16:11:54 +0545

    Relation added on user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,14 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function student()
+    {
+        return $this->belongsTo(Student::class);
+    }
+
+    
+    public function teacher(){
+        return $this->belongsTo(Teacher::class);
+    }
 }
