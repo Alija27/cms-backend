c5c1450b7a5676706b551c5d5eeb51a61f988cf3
Alija Ghimire <alijaghimire27@gmail.com>
2023-05-24 16:13:09 +0545

    Route created for api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\API\Auth\AuthController;
+use App\Http\Controllers\API\DepartmentController;
+use App\Http\Controllers\API\SemesterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +21,27 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post("login",[\App\Http\Controllers\API\Auth\AuthController::class,"login"]);
-Route::post("register",[\App\Http\Controllers\API\Auth\AuthController::class,"register"]);
+//Authentication
+Route::post("login", [\App\Http\Controllers\API\Auth\AuthController::class, "login"]);
+Route::post("register", [\App\Http\Controllers\API\Auth\AuthController::class, "register"]);
 
+//Students
+Route::resource("students", \App\Http\Controllers\API\StudentController::class);
+
+//Department
+Route::resource("departments", \App\Http\Controllers\API\DepartmentController::class);
+
+//Course
+Route::resource("courses", \App\Http\Controllers\API\CourseController::class);
+
+//Semester
+Route::resource("semesters", \App\Http\Controllers\API\SemesterController::class);
+
+//Batch
+Route::resource("batches", \App\Http\Controllers\API\BatchController::class);
+
+//Subject
+Route::resource("subjects", \App\Http\Controllers\API\SubjectController::class);
+
+//Teacher
+Route::resource("teachers", \App\Http\Controllers\API\TeacherController::class);
