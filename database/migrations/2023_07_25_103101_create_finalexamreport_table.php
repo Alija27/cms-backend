4be2fc1cde1591f46279b06cf2d3f905cad3c635
Alija Ghimire <alijaghimire27@gmail.com>
2023-07-25 21:13:01 +0545

    done FinalExamREport

---
+++ database/migrations/2023_07_25_103101_create_finalexamreport_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('finalexamreport', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId("course_id")->constrained("courses")->onDelete("cascade");
+            $table->foreignId("semester_id")->constrained("semesters")->onDelete("cascade");
+            $table->foreignId("student_id")->constrained("students")->onDelete("cascade");
+            $table->foreignId("batch_id")->constrained("batches")->onDelete("cascade");
+            $table->date("date");
+            $table->string("position")->nullable();
+            $table->string("grade")->nullable();
+            $table->string("gpa");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('finalexamreport');
+    }
+};
