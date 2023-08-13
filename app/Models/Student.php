b99c6efd59aab6587346565f5afd8363b4d171a6
Alija Ghimire <alijaghimire27@gmail.com>
2023-08-13 08:44:51 +0545

    Relation to department and batch

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -23,4 +23,12 @@ class Student extends Model
     public function course(){
         return $this->belongsTo(Course::class);
     }
+
+    public function department(){
+        return $this->belongsTo(Department::class);
+    }
+
+    public function batch(){
+        return $this->belongsTo(Batch::class);
+    }
 }
