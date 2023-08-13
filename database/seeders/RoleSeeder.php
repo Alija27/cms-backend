47b0c941098da5f4918ec28a392d228e39752106
Alija Ghimire <alijaghimire27@gmail.com>
2023-08-13 08:45:07 +0545

    update

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -14,30 +14,31 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         Role::create([
-            "name"=>"admin",
-            "guard_name"=>"web",
-        ]);
-        
-        Role::create([
-            "name"=>"accountant",
-            "guard_name"=>"web"
+            "name" => "admin",
+            "guard_name" => "web",
         ]);
 
         Role::create([
-            "name"=>"librarian",
-            "guard_name"=>"web",
+            "name" => "accountant",
+            "guard_name" => "web"
         ]);
-        
         Role::create([
-            "name"=>"teacher",
-            "guard_name"=>"web",
+            "name" => "examiner",
+            "guard_name" => "web",
+        ]);
+        Role::create([
+            "name" => "librarian",
+            "guard_name" => "web",
         ]);
 
         Role::create([
-            "name"=>"student",
-            "guard_name"=>"web",
+            "name" => "teacher",
+            "guard_name" => "web",
         ]);
-        
-        
+
+        Role::create([
+            "name" => "student",
+            "guard_name" => "web",
+        ]);
     }
 }
